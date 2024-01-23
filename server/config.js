70c2dcb5ba659a8c2b8a326d0f236e04f20eca30
JedBautista <39817307+JedBautista@users.noreply.github.com>
2024-01-23 15:21:35 +0800

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 let config = {
-	dbUrl:
-		"mongodb+srv://jpeb:FrhYkkZRaY0927dt@cluster1.sww3ihy.mongodb.net/?retryWrites=true&w=majority"
+	dbUrl: // put db url here
 };
 
 module.exports = config;
